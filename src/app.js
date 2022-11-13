1b86606cfe613115e6eacfdfcdb7f4a20b79fb55
tatz23 <tatianajesus23@sapo.pt>
2022-11-13 21:40:36 +0000

    edited js and fixed a few html problems

--- src/app.js
+++ src/app.js
@@ -28,4 +28,61 @@ function formatDay(timestamp) {
 
   return days[day];
 }
-let apiKey = "2afbc670a6b48bo2065e3872ftab04ec";
+
+//let apiKey = "2afbc670a6b48bo2065e3872ftab04ec";
+//let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query={city}&key=${apiKey}&units=metric`;
+
+function showWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#currrent-temperature").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.temperature.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "2afbc670a6b48bo2065e3872ftab04ec";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query={city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-form").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "2afbc670a6b48bo2065e3872ftab04ec";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#city-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let fahrenheitButton = document.querySelector("#fahrenheit-button");
+fahrenheitButton.addEventListener("click", celsiusToFahrenheit);
+
+let celsiusButton = document.querySelector("#celsius-button");
+celsiusButton.addEventListener("click", fahrenheitToCelsius);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Porto");
