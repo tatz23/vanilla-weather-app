e8f399010c638bf1f9d5e936d6effc43637ae7e3
tatz23 <tatianajesus23@sapo.pt>
2022-11-15 23:11:43 +0000

    updated the js
    changed the way my showWeatherCondition function looked and still working on unit conversion functions

--- src/app.js
+++ src/app.js
@@ -33,21 +33,22 @@ function formatDay(timestamp) {
 //let apiUrl = `https://api.shecodes.io/weather/v1/current?query={city}&key=${apiKey}&units=metric`;
 
 function showWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.city;
-  document.querySelector("#current-temperature").innerHTML = Math.round(
-    response.data.temperature.current
-  );
-  document.querySelector("#weather-description").innerHTML =
-    response.data.condition.description;
 
-  document.querySelector("#humidity").innerHTML =
-    response.data.temperature.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector(".feels-like").innerHTML = Math.round(
-    response.data.temperature.feels_like
-  );
+  document.querySelector("#city").innerHTML = response.data.city;
+  let currentTemperature= document.querySelector("#current-temperature");
+  let weatherDescription = document.querySelector("#weather-description");
+  let humidityElement= document.querySelector("#humidity");
+  let windElement= document.querySelector("#wind");
+  let feelsLike= document.querySelector(".feels-like");
+  let weatherIcon = document.querySelector("#icon");
+  let iconImage = response.data.condition.icon_url;
+
+  currentTemperature.innerHTML =Math.round(response.data.temperature.current);
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML =response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelsLike.innerHTML = Math.round(response.data.temperature.feels_like);
+  weatherIcon.setAttribute("src", iconImage);
 }
 
 function searchCity(city) {
@@ -73,6 +74,19 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+
+function showCelsius(event) {
+  event.preventDefault();
+let celsiusElement=document.querySelector("#current-temperature");
+celsiusElement.innerHTML=Math.round(repsonse.data.temperature.main)
+}
+
+//function showFahrenheit (event){
+//  event.preventDefault();
+// let fahrenheitElement = (response.data.temperature.main)*4
+// .5 + 32;
+//
+//}
 
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
