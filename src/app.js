917b9fe70a483afb88ed955cdaade57b04fee64d
tatz23 <114620564+tatz23@users.noreply.github.com>
2022-11-15 12:16:47 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ function searchCity(city) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city-input").value;
+  let city = document.querySelector("#city-form").value;
   searchCity(city);
 }
 
