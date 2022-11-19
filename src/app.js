4d6dfee80fb43f3bf376634227de0f0ac132953a
tatz23 <tatianajesus23@sapo.pt>
2022-11-19 16:23:29 +0000

    added the Forecast functions to the app
    js won't inject so forecast function isn't working properly

--- src/app.js
+++ src/app.js
@@ -29,8 +29,42 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-//let apiKey = "2afbc670a6b48bo2065e3872ftab04ec";
-//let apiUrl = `https://api.shecodes.io/weather/v1/current?query={city}&key=${apiKey}&units=metric`;
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                <div class="weather-forecast-date">${formatDay(forecastDay.time)} </div>
+                <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                    forecastDay.condition.icon}.png"
+                  alt="" width="42"/>
+                <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-maximum">${Math.round(forecastDay.temperature.maximum)}°</span>
+                <span class="weather-forecast-temperature-minimum">${Math.round(forecastDay.temperature.minimum)}°</span>
+              </div>
+          
+        </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast (coordinates) {
+  let apiKey ="2afbc670a6b48bo2065e3872ftab04ec";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
 
 function showWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.city;
@@ -115,3 +149,6 @@ let fahrenheitButton = document.querySelector("#fahrenheit-button");
 fahrenheitButton.addEventListener("click", showFahrenheit);
 
 searchCity("Porto");
+
+//let apiKey = "2afbc670a6b48bo2065e3872ftab04ec";
+//let apiUrl = `https://api.shecodes.io/weather/v1/current?query={city}&key=${apiKey}&units=metric`;
